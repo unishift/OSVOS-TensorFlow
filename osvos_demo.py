e24b1b35c0600c7a52e9f73430ea327677f14be1
unishift <30509893+unishift@users.noreply.github.com>
2019-02-26 22:32:00 +0300

    Fix for tests in colab

--- osvos_demo.py
+++ osvos_demo.py
@@ -23,10 +23,11 @@ from dataset import Dataset
 os.chdir(root_folder)
 
 # User defined parameters
-seq_name = "car-shadow"
+seq_name = 'karting' 
 gpu_id = 0
-train_model = True
-result_path = os.path.join('DAVIS', 'Results', 'Segmentations', '480p', 'OSVOS', seq_name)
+train_model = False
+result_path = 'Results'
+# result_path = os.path.join('DAVIS', 'Results', 'Segmentations', '480p', 'OSVOS', seq_name)
 
 # Train parameters
 parent_path = os.path.join('models', 'OSVOS_parent', 'OSVOS_parent.ckpt-50000')
@@ -34,8 +35,9 @@ logs_path = os.path.join('models', seq_name)
 max_training_iters = 500
 
 # Define Dataset
-test_frames = sorted(os.listdir(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name)))
-test_imgs = [os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, frame) for frame in test_frames]
+# test_frames = sorted(os.listdir(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name)))
+test_imgs = 'test.txt'
+# test_imgs = [os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, frame) for frame in test_frames]
 if train_model:
     train_imgs = [os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, '00000.jpg')+' '+
                   os.path.join('DAVIS', 'Annotations', '480p', seq_name, '00000.png')]
@@ -63,20 +65,20 @@ with tf.Graph().as_default():
         osvos.test(dataset, checkpoint_path, result_path)
 
 # Show results
-overlay_color = [255, 0, 0]
-transparency = 0.6
-plt.ion()
-for img_p in test_frames:
-    frame_num = img_p.split('.')[0]
-    img = np.array(Image.open(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, img_p)))
-    mask = np.array(Image.open(os.path.join(result_path, frame_num+'.png')))
-    mask = mask//np.max(mask)
-    im_over = np.ndarray(img.shape)
-    im_over[:, :, 0] = (1 - mask) * img[:, :, 0] + mask * (overlay_color[0]*transparency + (1-transparency)*img[:, :, 0])
-    im_over[:, :, 1] = (1 - mask) * img[:, :, 1] + mask * (overlay_color[1]*transparency + (1-transparency)*img[:, :, 1])
-    im_over[:, :, 2] = (1 - mask) * img[:, :, 2] + mask * (overlay_color[2]*transparency + (1-transparency)*img[:, :, 2])
-    plt.imshow(im_over.astype(np.uint8))
-    plt.axis('off')
-    plt.show()
-    plt.pause(0.01)
-    plt.clf()
+# overlay_color = [255, 0, 0]
+# transparency = 0.6
+# plt.ion()
+# for img_p in test_frames:
+#     frame_num = img_p.split('.')[0]
+#     img = np.array(Image.open(os.path.join('DAVIS', 'JPEGImages', '480p', seq_name, img_p)))
+#     mask = np.array(Image.open(os.path.join(result_path, frame_num+'.png')))
+#     mask = mask//np.max(mask)
+#     im_over = np.ndarray(img.shape)
+#     im_over[:, :, 0] = (1 - mask) * img[:, :, 0] + mask * (overlay_color[0]*transparency + (1-transparency)*img[:, :, 0])
+#     im_over[:, :, 1] = (1 - mask) * img[:, :, 1] + mask * (overlay_color[1]*transparency + (1-transparency)*img[:, :, 1])
+#     im_over[:, :, 2] = (1 - mask) * img[:, :, 2] + mask * (overlay_color[2]*transparency + (1-transparency)*img[:, :, 2])
+#     plt.imshow(im_over.astype(np.uint8))
+#     plt.axis('off')
+#     plt.show()
+#     plt.pause(0.01)
+#     plt.clf()
